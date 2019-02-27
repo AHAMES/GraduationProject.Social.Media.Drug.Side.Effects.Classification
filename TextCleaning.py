60e17c391b98ee00d29240787406fc88e56e0182
Ahmed ElSabbagh <Ahmed.h.elsabbgh@gmail.com>
2019-02-27 14:41:47 +0200

    text cleaning now has new column and does not override other texts

--- TextCleaning.py
+++ TextCleaning.py
@@ -21,17 +21,30 @@ posts= pandas.read_csv('posts.csv')
 for i in range(0,len(posts)-1):
     
     stop_words =  set(stopwords.words('english'))
-
-    word_tokens = word_tokenize(posts.iloc[i]['Content']) 
+    newSentence =""
+    sentence=posts.iloc[i]['Content']
+    for k in sentence:
+        if k.isalpha() or k.isdigit() or k==' ':
+            newSentence=newSentence+k
+            continue
+        else:
+            continue
+    
+    word_tokens = word_tokenize(newSentence) 
     filtered_sentence = [] 
     for w in word_tokens: 
+        w=w.replace('!'," ")
+        w=w.replace(';'," ")
+        w=w.replace("’"," ") 
+        w=w.replace("'"," ")
+        w=w.replace('?'," ")
         if w not in stop_words: 
             filtered_sentence.append(str(stemmer.stem(w))) 
         
         tokens=' '.join(map(str,word_tokens))
         filtered=' '.join(map(str,filtered_sentence)) 
         filtered=filtered.translate(None, string.punctuation)
-        posts.at[i , 'Content'] = filtered
+        posts.at[i , 'Filtered'] = filtered
 
 listOfPosts=posts.iloc[:]['Content']
 bagOfWords = vectorizer.fit(listOfPosts.tolist())
